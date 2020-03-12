4b91aa9ef01c51d59c5c5a7fa30af79fe0600362
Mark Koelle <mkoelle@gmail.com>
2020-03-12 02:00:03 -0500

    add config, env variables

--- src/shared/data.ts
+++ src/shared/data.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 import { Resource } from "./interfaces";
+import { API_URL } from "./config";
 
 const getResources = async function(): Promise<Array<Resource>> {
-  const response = await axios.get<Array<Resource>>("api/resources.json");
+  const response = await axios.get<Array<Resource>>(
+    `${API_URL}/resources.json`
+  );
   return response.data;
 };
 
---
+++ src/shared/index.ts
@@ -0,0 +1,3 @@
+export * from "./data";
+export * from "./interfaces";
+export * from "./config";

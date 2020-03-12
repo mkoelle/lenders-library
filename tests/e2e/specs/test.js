47591a4afdb79e16bd41bf6023c8e9ec638386f4
Mark Koelle <mark.koelle@vwcredit.com>
2020-03-12 10:43:28 -0500

    rename

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,6 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to the Community Library!");
+    cy.contains("h1", "Welcome to the Lenders Library!");
   });
 });

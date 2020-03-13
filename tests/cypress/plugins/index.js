eeb6600a78acd63feeb5aba93ba0e7f9463da97a
mark koelle <mkoelle@gmail.com>
2020-03-13 14:37:59 -0500

    moved cypress tests

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -16,10 +16,10 @@ module.exports = (on, config) => {
   // }))
 
   return Object.assign({}, config, {
-    fixturesFolder: "tests/e2e/fixtures",
-    integrationFolder: "tests/e2e/specs",
-    screenshotsFolder: "tests/e2e/screenshots",
-    videosFolder: "tests/e2e/videos",
-    supportFile: "tests/e2e/support/index.js"
+    "supportFile": "tests/cypress/support/index.js",
+    "fixturesFolder": "tests/cypress/fixtures",
+    "integrationFolder": "tests/cypress/specs",
+    "screenshotsFolder": "tests/cypress/screenshots",
+    "videosFolder": "tests/cypress/videos"
   });
 };

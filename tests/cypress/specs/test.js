32aa662e0ff7f3d9bac8fbc71b9090d2bf9f83c4
mark koelle <mkoelle@gmail.com>
2020-03-16 10:46:19 -0500

    cypress running again

--- tests/cypress/specs/test.js
+++ tests/cypress/specs/test.js
@@ -3,6 +3,6 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to the Lenders Library!");
+    cy.contains("p.title", "Library");
   });
 });
